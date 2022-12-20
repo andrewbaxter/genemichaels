6f54bb200f15b16c0a94c5b4f4dfde7e79b397c2
Billy Sheppard <billyoptussheppard@gmail.com>
2022-12-20 03:26:45 +0000

    println cleanup

--- src/bin/genemichaels.rs
+++ src/bin/genemichaels.rs
@@ -1,10 +1,6 @@
-use anyhow::{
-    anyhow,
-    Result,
-};
+use anyhow::anyhow;
 use clap::Parser;
 use genemichaels::{
-    es,
     format_str,
     FormatConfig,
     Comment,
@@ -18,160 +14,164 @@ use std::{
     process,
     str::FromStr,
     fmt::Display,
+    result,
 };
 use syn::File;
 
-fn main() {
-    #[derive(Clone)]
-    enum Offable<T> {
-        Off,
-        On(T),
-    }
+type Result<T> = result::Result<T, anyhow::Error>;
 
-    impl<T: Display> std::fmt::Display for Offable<T> {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            match self {
-                Offable::Off => "off".fmt(f),
-                Offable::On(x) => x.fmt(f),
-            }
+#[derive(Clone)]
+enum Offable<T> {
+    Off,
+    On(T),
+}
+
+impl<T: Display> std::fmt::Display for Offable<T> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Offable::Off => "off".fmt(f),
+            Offable::On(x) => x.fmt(f),
         }
     }
+}
 
-    impl<
-        E: std::error::Error + Send + Sync + 'static,
-        T: FromStr<Err = E> + Clone + Display,
-    > FromStr for Offable<T> {
-        type Err = anyhow::Error;
+impl<E: std::error::Error + Send + Sync + 'static, T: FromStr<Err = E> + Clone + Display> FromStr for Offable<T> {
+    type Err = anyhow::Error;
 
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            if s == "off" {
-                Ok(Self::Off)
-            } else {
-                Ok(Self::On(T::from_str(s)?))
-            }
+    fn from_str(s: &str) -> result::Result<Self, Self::Err> {
+        if s == "off" {
+            Ok(Self::Off)
+        } else {
+            Ok(Self::On(T::from_str(s)?))
         }
     }
-    #[derive(Clone)]
-    struct On;
+}
+#[derive(Clone)]
+struct On;
 
-    impl std::fmt::Display for On {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            "on".fmt(f)
+impl std::fmt::Display for On {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        "on".fmt(f)
+    }
+}
+
+#[derive(Debug)]
+struct OnErr(String);
+
+impl Display for OnErr {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        self.0.fmt(f)
+    }
+}
+
+impl std::error::Error for OnErr {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        None
+    }
+}
+
+impl FromStr for On {
+    type Err = OnErr;
+
+    fn from_str(s: &str) -> result::Result<Self, Self::Err> {
+        if s == "on" {
+            Ok(On)
+        } else {
+            Err(OnErr(format!("[{}] not allowed, must be on or off", s)))
         }
     }
+}
 
-    #[derive(Debug)]
-    struct OnErr(String);
+#[derive(Parser)]
+struct Args {
+    #[arg(help = "Files to format in place; if none specified formats stdin and writes result to stdout")]
+    files: Vec<PathBuf>,
+    #[arg(short, long, default_value_t = FormatConfig::default().max_width)]
+    line_length: usize,
+    #[arg(short, long, help = "Won't emit any output")]
+    quiet: bool,
+    #[arg(long, help = "For any node that's split, all parent nodes must also be split")]
+    root_splits: bool,
+    #[arg(
+        long,
+        help = "Always split {} groups with >= this number of children; disable with `off`",
+        default_value_t = match FormatConfig::default().split_brace_threshold {
+            Some(x) => Offable::On(x),
+            None => Offable::Off,
+        },
+    )]
+    split_brace_threshold: Offable<usize>,
+    #[arg(
+        long,
+        help = "Always split #[] attributes; disable with `off`",
+        default_value_t = match FormatConfig::default().split_attributes {
+            true => Offable::On(On),
+            false => Offable::Off,
+        },
+    )]
+    split_attributes: Offable<On>,
+    #[arg(
+        long,
+        help = "Always split where clauses; disable with `off`",
+        default_value_t = match FormatConfig::default().split_attributes {
+            true => Offable::On(On),
+            false => Offable::Off,
+        },
+    )]
+    split_where: Offable<On>,
+    #[arg(
+        long,
+        help = "Use a max comment length relative to start of comment (i.e. ignoring indentation); disable with `off`",
+        default_value_t = match FormatConfig::default().comment_width {
+            Some(x) => Offable::On(x),
+            None => Offable::Off,
+        },
+    )]
+    comment_length: Offable<usize>,
+    #[arg(
+        long,
+        help = "Problems formatting comments are fatal; disable with `false`",
+        default_value_t = match FormatConfig::default().comment_errors_fatal {
+            true => Offable::On(On),
+            false => Offable::Off,
+        },
+    )]
+    comment_errors_fatal: Offable<On>,
+}
 
-    impl Display for OnErr {
-        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-            self.0.fmt(f)
-        }
+fn skip(src: &str) -> bool {
+    src.lines().take(5).any(|l| l.contains("`nogenemichaels`"))
+}
+
+fn process(config: &FormatConfig, source: &str) -> Result<String> {
+    let res = format_str(source, config)?;
+    if !res.lost_comments.is_empty() {
+        return Err(
+            anyhow!(
+                "The following comments were missed during formatting: {:?}",
+                res.lost_comments.values().flatten().collect::<Vec<&Comment>>()
+            ),
+        );
     }
-
-    impl std::error::Error for OnErr {
-        fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
-            None
-        }
-    }
-
-    impl FromStr for On {
-        type Err = OnErr;
-
-        fn from_str(s: &str) -> Result<Self, Self::Err> {
-            if s == "on" {
-                Ok(On)
-            } else {
-                return Err(OnErr(format!("[{}] not allowed, must be on or off", s)))
-            }
-        }
-    }
-
-    #[derive(Parser)]
-    struct Args {
-        #[arg(help = "Files to format in place; if none specified formats stdin and writes result to stdout")]
-        files: Vec<PathBuf>,
-        #[arg(short, long, default_value_t = FormatConfig::default().max_width)]
-        line_length: usize,
-        #[arg(long, help = "For any node that's split, all parent nodes must also be split")]
-        root_splits: bool,
-        #[arg(
-            long,
-            help = "Always split {} groups with >= this number of children; disable with `off`",
-            default_value_t = match FormatConfig::default().split_brace_threshold {
-                Some(x) => Offable::On(x),
-                None => Offable::Off,
-            },
-        )]
-        split_brace_threshold: Offable<usize>,
-        #[arg(
-            long,
-            help = "Always split #[] attributes; disable with `off`",
-            default_value_t = match FormatConfig::default().split_attributes {
-                true => Offable::On(On),
-                false => Offable::Off,
-            },
-        )]
-        split_attributes: Offable<On>,
-        #[arg(
-            long,
-            help = "Always split where clauses; disable with `off`",
-            default_value_t = match FormatConfig::default().split_attributes {
-                true => Offable::On(On),
-                false => Offable::Off,
-            },
-        )]
-        split_where: Offable<On>,
-        #[arg(
-            long,
-            help =
-                "Use a max comment length relative to start of comment (i.e. ignoring indentation); disable with `off`",
-            default_value_t = match FormatConfig::default().comment_width {
-                Some(x) => Offable::On(x),
-                None => Offable::Off,
-            },
-        )]
-        comment_length: Offable<usize>,
-        #[arg(
-            long,
-            help = "Problems formatting comments are fatal; disable with `false`",
-            default_value_t = match FormatConfig::default().comment_errors_fatal {
-                true => Offable::On(On),
-                false => Offable::Off,
-            },
-        )]
-        comment_errors_fatal: Offable<On>,
-    }
-
-    let args = Args::parse();
-
-    fn process(config: &FormatConfig, source: &str) -> Result<String> {
-        let res = format_str(source, config)?;
-        if !res.lost_comments.is_empty() {
+    match syn::parse_str::<File>(&res.rendered) {
+        Ok(_) => { },
+        Err(e) => {
+            eprintln!("{}", res.rendered);
             return Err(
                 anyhow!(
-                    "The following comments were missed during formatting: {:?}",
-                    res.lost_comments.values().flatten().collect::<Vec<&Comment>>()
+                    "Rendered document couldn't be re-parsed in verification step: {}:{}: {}",
+                    e.span().start().line,
+                    e.span().start().column,
+                    e
                 ),
             );
-        }
-        match syn::parse_str::<File>(&res.rendered) {
-            Ok(_) => { },
-            Err(e) => {
-                eprintln!("{}", res.rendered);
-                return Err(
-                    anyhow!(
-                        "Rendered document couldn't be re-parsed in verification step: {}:{}: {}",
-                        e.span().start().line,
-                        e.span().start().column,
-                        e
-                    ),
-                );
-            },
-        };
-        Ok(res.rendered)
-    }
+        },
+    };
+    Ok(res.rendered)
+}
 
+fn main() {
+    let args = Args::parse();
     let config = FormatConfig {
         max_width: args.line_length,
         root_splits: args.root_splits,
@@ -196,24 +196,21 @@ fn main() {
             Offable::On(_) => true,
         },
     };
-
-    fn skip(src: &str) -> bool {
-        src.lines().take(5).any(|l| l.contains("`nogenemichaels`"))
-    }
-
     if args.files.is_empty() {
-        match es!({
+        let res = || -> Result<()> {
             let mut source = Vec::new();
             std::io::stdin().read_to_end(&mut source)?;
             let source = String::from_utf8(source)?;
             if skip(&source) {
                 print!("{}", source);
-                return Ok(());
+                anyhow::Ok(())
+            } else {
+                let out = process(&config, &source)?;
+                print!("{}", out);
+                anyhow::Ok(())
             }
-            let out = process(&config, &source)?;
-            print!("{}", out);
-            Ok(())
-        }) {
+        };
+        match res() {
             Ok(_) => { },
             Err(e) => {
                 eprintln!("Error formatting stdin: {:?}", e);
@@ -223,7 +220,7 @@ fn main() {
     } else {
         let mut failed = false;
         for file in &args.files {
-            match es!({
+            let res = || -> Result<()> {
                 let source = String::from_utf8(fs::read(file)?)?;
                 if skip(&source) {
                     eprintln!("Skipping {}", &file.to_string_lossy());
@@ -233,7 +230,8 @@ fn main() {
                 let out = process(&config, &source)?;
                 fs::write(file, out.as_bytes())?;
                 Ok(())
-            }) {
+            };
+            match res() {
                 Ok(_) => { },
                 Err(e) => {
                     eprintln!("Error formatting {}: {:?}", &file.to_string_lossy(), e);
